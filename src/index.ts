5199d82ff7f45b0179b10056b2bfc35238002fb8
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-09 07:48:48 +0000

    Ready for main if statements

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,25 @@ const inquirer = require("inquirer");
 const path = require('path');
 const { writeFile, readdir, readFile } = require("fs").promises;
 
-const configFiles = {};
+const configFiles : any = {};
 const configFolderPath = path.resolve(__dirname, "config")
 
 (async () => {
     const files = await readdir(configFolderPath).catch(console.log)
 
     for (let i of files) {
-        const frameWorkName
+        const fileType = i.split("."[1])
+        configFiles[fileType] = path.join(configFolderPath, i);
     }
+
+    const { file } = await inquirer.prompt([
+        {
+            type: "list",
+            message: "What Code Boilerplate do you want to generate?",
+            name: "file",
+            choices: Object.keys(configFiles),
+        }
+    ]);
+
+    var config = await readFile(configFiles[file].catch(console.log));
 })
