20b05c6c3478e40efae012897c048043f6c6baf7
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-02-10 18:44:00 +0000

    Update package.json

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    endOfLine: 'auto',
+    trailingComma: 'all',
+    overrides: [
+        {
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
+};
