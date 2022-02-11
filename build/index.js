6d66b43a20cd4ca1adfeaacb920a00bcff94f8d8
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-11 18:43:36 +0000

    Code is generated for the C Language!

--- build/index.js
+++ build/index.js
@@ -3,101 +3,30 @@
 const inquirer = require("inquirer");
 const path = require('path');
 const { writeFile, readdir, readFile } = require("fs").promises;
-const fs = require("fs");
 const configFiles = {};
-const configFolderPath = path.resolve(__dirname, "config");
-const files = readdir(configFolderPath).catch(console.log);
-for (const file of Object.keys(files)) {
-    configFiles[file] = readFile(path.resolve(configFolderPath, file)).catch(console.log);
-}
-const { file } = inquirer.prompt([
-    {
-        type: "list",
-        message: "What Code Boilerplate do you want to generate?",
-        name: "file",
-        choices: Object.keys(configFiles)
+const configFolderPath = path.resolve(__dirname, 'config');
+(async () => {
+    const files = await readdir(configFolderPath).catch(console.log);
+    for (let i of files) {
+        const frameworkName = i.split('.')[1];
+        configFiles[frameworkName] = path.join(configFolderPath, i);
     }
-]);
-switch (file) {
-    case "C":
-        const cDefaultConfigFile = fs.readFileSync(`${__dirname}/config/c.c`, "utf8");
-        const cUpdateConfigFile = async (fileName) => {
-            const configFile = await readFile(configFiles[fileName], "utf8");
-            const newConfigFile = configFile.replace(/\{\{fileName\}\}/g, fileName);
-            await writeFile(fileName, newConfigFile);
-        };
-        cUpdateConfigFile(file);
-        break;
-    case "C++":
-        const cppDefaultConfigFile = fs.readFileSync(`${__dirname}/config/cpp.cpp`, "utf8");
-        const cppUpdateConfigFile = async (fileName) => {
-            const configFile = await readFile(configFiles[fileName], "utf8");
-            const newConfigFile = configFile.replace(/\{\{fileName\}\}/g, fileName);
-            await writeFile(fileName, newConfigFile);
-        };
-        cppUpdateConfigFile(file);
-        break;
-    case "Java":
-        const javaDefaultConfigFile = fs.readFileSync(`${__dirname}/config/java.java`, "utf8");
-        const javaUpdateConfigFile = async (fileName) => {
-            const configFile = await readFile(configFiles[fileName], "utf8");
-            const newConfigFile = configFile.replace(/\{\{fileName\}\}/g, fileName);
-            await writeFile(fileName, newConfigFile);
-        };
-        javaUpdateConfigFile(file);
-        break;
-    case "C#":
-        const csharpDefaultConfigFile = fs.readFileSync(`${__dirname}/config/csharp.cs`, "utf8");
-        const csharpUpdateConfigFile = async (fileName) => {
-            const configFile = await readFile(configFiles[fileName], "utf8");
-            const newConfigFile = configFile.replace(/\{\{fileName\}\}/g, fileName);
-            await writeFile(fileName, newConfigFile);
-        };
-        csharpUpdateConfigFile(file);
-        break;
-    case "Python":
-        const pythonDefaultConfigFile = fs.readFileSync(`${__dirname}/config/python.py`, "utf8");
-        const pythonUpdateConfigFile = async (fileName) => {
-            const configFile = await readFile(configFiles[fileName], "utf8");
-            const newConfigFile = configFile.replace(/\{\{fileName\}\}/g, fileName);
-            await writeFile(fileName, newConfigFile);
-        };
-        pythonUpdateConfigFile(file);
-        break;
-    case "Golang":
-        const golangDefaultConfigFile = fs.readFileSync(`${__dirname}/config/golang.go`, "utf8");
-        const golangUpdateConfigFile = async (fileName) => {
-            const configFile = await readFile(configFiles[fileName], "utf8");
-            const newConfigFile = configFile.replace(/\{\{fileName\}\}/g, fileName);
-            await writeFile(fileName, newConfigFile);
-        };
-        golangUpdateConfigFile(file);
-        break;
-    case "HTML":
-        const htmlDefaultConfigFile = fs.readFileSync(`${__dirname}/config/html.html`, "utf8");
-        const htmlUpdateConfigFile = async (fileName) => {
-            const configFile = await readFile(configFiles[fileName], "utf8");
-            const newConfigFile = configFile.replace(/\{\{fileName\}\}/g, fileName);
-            await writeFile(fileName, newConfigFile);
-        };
-        htmlUpdateConfigFile(file);
-        break;
-    case "JavaScript":
-        const jsDefaultConfigFile = fs.readFileSync(`${__dirname}/config/javascript.js`, "utf8");
-        const jsUpdateConfigFile = async (fileName) => {
-            const configFile = await readFile(configFiles[fileName], "utf8");
-            const newConfigFile = configFile.replace(/\{\{fileName\}\}/g, fileName);
-            await writeFile(fileName, newConfigFile);
-        };
-        jsUpdateConfigFile(file);
-        break;
-    case "TypeScript":
-        const tsDefaultConfigFile = fs.readFileSync(`${__dirname}/config/typescript.ts`, "utf8");
-        const tsUpdateConfigFile = async (fileName) => {
-            const configFile = await readFile(configFiles[fileName], "utf8");
-            const newConfigFile = configFile.replace(/\{\{fileName\}\}/g, fileName);
-            await writeFile(fileName, newConfigFile);
-        };
-        tsUpdateConfigFile(file);
-        break;
-}
+    const { language } = await inquirer.prompt([
+        {
+            type: "list",
+            message: "Which Language do you want to use:",
+            name: "language",
+            choices: Object.keys(configFiles),
+        }
+    ]);
+    switch (language) {
+        case "c":
+            let config = await readFile(configFiles[language]).catch(console.log);
+            const filePath = path.join(process.cwd(), 'c.c');
+            await writeFile(filePath, config.toString()).catch((err) => {
+                console.log(err);
+                process.exit();
+            });
+    }
+    console.log("Code Boilerplate generated for the ", language, " language");
+})();
